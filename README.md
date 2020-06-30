b5a348463848322f104005f460ec37e76681e3f5
Felipe Constantino <felipe.constantino@apliplaybook.com>
2020-06-30 18:32:13 -0300

    commit main boilerplate

---
+++ README.md
@@ -0,0 +1,13 @@
+# How to get this boilerplate
+
+```bash
+npx degit https://github.com/apiplaybook/svelte-boilerplate.git my-project-folder
+```
+OR for SSH
+```bash
+npx degit git@github.com:apiplaybook/svelte-boilerplate.git my-project-folder
+```
+
+Comes with TypeScript enabled (you can import `.ts` modules on `.svelte`, and vice-versa)
+
+If using VS Code, be sure to use [Svelte for VS Code](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode) extension
---
+++ rollup.config.js
@@ -0,0 +1,75 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import css from 'rollup-plugin-css-only';
+import livereload from 'rollup-plugin-livereload';
+import typescript from 'rollup-plugin-typescript2';
+import graphql from 'rollup-plugin-graphql';
+import preprocess from 'svelte-preprocess';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	input: 'src/main.ts',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: 'public/build/bundle.js'
+	},
+	plugins: [
+		svelte({
+			// enable run-time checks when not in production
+			dev: !production,
+			// we'll extract any component CSS out into
+			// a separate file - better for performance
+			css: css => {
+				css.write('public/build/bundle.css');
+			},
+			preprocess: preprocess()
+		}),
+
+		typescript(),
+		resolve({
+			browser: true,
+			dedupe: ['svelte'],
+			preferBuiltins: false
+		}),
+		graphql(),
+		css({ output: 'public/build/imported.bundle.css' }),
+		commonjs({ include: "node_modules/**", extensions: [".js", ".ts"] }),
+
+		// In dev mode, call `npm run start` once
+		// the bundle has been generated
+		!production && serve(),
+
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('public'),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
+	}
+};
+
+function serve() {
+	let started = false;
+
+	return {
+		writeBundle() {
+			if (!started) {
+				started = true;
+
+				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+					stdio: ['ignore', 'inherit', 'inherit'],
+					shell: true
+				});
+			}
+		}
+	};
+}
